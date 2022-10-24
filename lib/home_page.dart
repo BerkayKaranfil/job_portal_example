c87ad47c2fc116308ff5dd7407a0544e9b7b6acd
BerkayKaranfil <berkaykaranfil97@gmail.com>
2022-10-24 09:58:21 +0300

    Bottom bar hariç hazır.

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:job_portal_example/HomePageWidgets/appbar.dart';
+import 'package:job_portal_example/HomePageWidgets/home_textformfield.dart';
+import 'package:job_portal_example/HomePageWidgets/listview_item.dart';
+import 'package:job_portal_example/items.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,6 +19,43 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Consumer(builder: ((context, items items, widget) {
+      return Scaffold(
+        backgroundColor: Color(0xff0C0D15),
+        body: Padding(
+          padding: const EdgeInsets.only(right: 20, left: 20),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 44,
+              ),
+              AppbarWidget(),
+              SizedBox(
+                height: 16,
+              ),
+              HomeTextFormFieldWidget(),
+              SizedBox(
+                height: 22,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("Popular Jobs", style: GoogleFonts.inter(fontSize: 17, color: Color(0xffF8FAFC), fontWeight: FontWeight.bold),),
+                  Text("See All", style: GoogleFonts.inter(fontSize: 15, color: Color(0xff404040)),)
+                ],
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Container(
+                height: 510,
+                width: double.infinity,
+                child: HomePageListViewWidget(),
+              )
+            ],
+          ),
+        ),
+      );
+    }));
   }
 }
